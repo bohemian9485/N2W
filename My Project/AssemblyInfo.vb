767a41a9d699b4a661d614d767ca553ab74d0f4a
bohemian9485 <tubianosaric26@gmail.com>
2022-11-23 11:30:16 +0800

    Add project files.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Resources
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Number to Words")>
+<Assembly: AssemblyDescription("Converts number to words")>
+<Assembly: AssemblyCompany("Reynaldo Ibarra C. Tubianosa")>
+<Assembly: AssemblyProduct("N2W.dll")>
+<Assembly: AssemblyCopyright("Reynaldo Ibarra C. Tubianosa Copyright ©  2022")>
+<Assembly: AssemblyTrademark("Reynaldo Ibarra C. Tubianosa")>
+
+<Assembly: ComVisible(True)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("2504ed32-fa8f-40bf-a73d-ca9781f5727b")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: NeutralResourcesLanguage("en-US")>
